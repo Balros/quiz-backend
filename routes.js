1688122152ac52a7a32cbbc4bb7e71b55507fde5
adam.grund@gmail.com <adam.grund@gmail.com>
2019-06-03 22:53:14 +0200

    transition to json-ld data

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ID = require("virtuoso-uid");
 const router = express.Router();
+const sparqlTransformer = require('sparql-transformer');
 
 const { Client, Node, Text, Data, Triple } = require("virtuoso-sparql-client");
 
@@ -49,57 +50,84 @@ router.post("/api/questionGroups", async (req, res) => {
   const isTeacher = req.body.token === "teacher";
   //TODO isTeacher should be determined by token of user
   // provisional token
-  try {
+  // try {
 
-    const resultsTeacher = [
-      {
-        id: "topicUri1",
-        topicLabel: "Topic name - with assignment",
-        assignment: {
-          id: 'questionAssignmentUri',
-          description: "desription text",
-          startTime: '2019-03-14',
-          endTime: '2020-12-15',
-          questions: {
-            approved: [
-              { id: "questionUri1", label: "this question label 1", approval: true, lastSeenTeacher: '2019-03-14', lastSeenStudent: '2019-03-15', lastChange: '2019-03-15' },
-              { id: "questionUri2", label: "this question label 2", approval: true, lastSeenTeacher: '2019-03-14', lastSeenStudent: '2019-03-15', lastChange: '2019-03-15' }
-            ],
-            notApproved: [
-              { id: "questionUri3", label: "this question label 3", approval: false, lastSeenTeacher: '2019-03-15', lastSeenStudent: '2019-03-14', lastChange: '2019-03-15' }
-            ]
-          }
-        },
-      },
-      {
-        id: "topicUri2",
-        topicLabel: "Topic name - without assignment"
-      }
-    ];
-    //get only assignments assigned to student
-    const resultsStudent = [
-      {
-        id: "topicUri1",
-        topicLabel: "Topic name - with assignment",
-        assignment: {
-          id: 'questionAssignmentUri',
-          description: "desription text",
-          startTime: '2019-03-14',
-          endTime: '2020-12-15',
-          questions: {
-            approved: [
-              { id: "questionUri1", label: "this question label 1", approval: true, lastSeenTeacher: '2019-03-14', lastSeenStudent: '2019-03-15', lastChange: '2019-03-15' },
-              { id: "questionUri2", label: "this question label 2", approval: true, lastSeenTeacher: '2019-03-14', lastSeenStudent: '2019-03-15', lastChange: '2019-03-15' }
-            ],
-            notApproved: [
-              { id: "questionUri3", label: "this question label 3", approval: false, lastSeenTeacher: '2019-03-15', lastSeenStudent: '2019-03-14', lastChange: '2019-03-15' }
-            ]
-          }
-        },
-      },
-    ];
-    console.log(isTeacher ? resultsTeacher : resultsStudent);
-    res.status(200).json(isTeacher ? resultsTeacher : resultsStudent);
+  //   const resultsTeacher = [
+  //     {
+  //       id: "topicUri1",
+  //       topicLabel: "Topic name - with assignment",
+  //       assignment: {
+  //         id: 'questionAssignmentUri',
+  //         description: "desription text",
+  //         startTime: '2019-03-14',
+  //         endTime: '2020-12-15',
+  //         questions: {
+  //           approved: [
+  //             { id: "questionUri1", label: "this question label 1", approval: true, lastSeenTeacher: '2019-03-14', lastSeenStudent: '2019-03-15', lastChange: '2019-03-15' },
+  //             { id: "questionUri2", label: "this question label 2", approval: true, lastSeenTeacher: '2019-03-14', lastSeenStudent: '2019-03-15', lastChange: '2019-03-15' }
+  //           ],
+  //           notApproved: [
+  //             { id: "questionUri3", label: "this question label 3", approval: false, lastSeenTeacher: '2019-03-15', lastSeenStudent: '2019-03-14', lastChange: '2019-03-15' }
+  //           ]
+  //         }
+  //       },
+  //     },
+  //     {
+  //       id: "topicUri2",
+  //       topicLabel: "Topic name - without assignment"
+  //     }
+  //   ];
+  //   //get only assignments assigned to student
+  //   const resultsStudent = [
+  //     {
+  //       id: "topicUri1",
+  //       topicLabel: "Topic name - with assignment",
+  //       assignment: {
+  //         id: 'questionAssignmentUri',
+  //         description: "desription text",
+  //         startTime: '2019-03-14',
+  //         endTime: '2020-12-15',
+  //         questions: {
+  //           approved: [
+  //             { id: "questionUri1", label: "this question label 1", approval: true, lastSeenTeacher: '2019-03-14', lastSeenStudent: '2019-03-15', lastChange: '2019-03-15' },
+  //             { id: "questionUri2", label: "this question label 2", approval: true, lastSeenTeacher: '2019-03-14', lastSeenStudent: '2019-03-15', lastChange: '2019-03-15' }
+  //           ],
+  //           notApproved: [
+  //             { id: "questionUri3", label: "this question label 3", approval: false, lastSeenTeacher: '2019-03-15', lastSeenStudent: '2019-03-14', lastChange: '2019-03-15' }
+  //           ]
+  //         }
+  //       },
+  //     },
+  //   ];
+    
+  //   console.log(isTeacher ? resultsTeacher : resultsStudent);
+  //   res.status(200).json(isTeacher ? resultsTeacher : resultsStudent);
+  // } catch (e) {
+  //   console.log(e);
+  //   res.send("Error!");
+  // }
+  const options = {
+    context: "http://schema.org",
+    endpoint: "http://localhost:8890/sparql",
+    debug: true
+  };
+  const q = {
+    "proto": [{
+      "id" : "?id",
+      "name": "?name",
+    }],
+    "$where": [
+      "?id rdf:type foaf:Topic",
+      "?id foaf:name ?name"
+    ],
+    "$prefixes": {
+      "foaf": "http://www.semanticweb.org/semanticweb#"
+    },
+    "$limit": 100
+  };
+  try {
+    const out = await sparqlTransformer.default(q, options);
+    res.status(200).json(out);
   } catch (e) {
     console.log(e);
     res.send("Error!");
@@ -107,17 +135,28 @@ router.post("/api/questionGroups", async (req, res) => {
 });
 
 router.get("/api/topics", async (req, res) => {
-  const query =
-    "SELECT ?subject ?name WHERE {?subject rdf:type foaf:Topic . ?subject foaf:name ?name}";
-  localClient.setQueryFormat(format);
-  localClient.setQueryGraph(graphName);
+  const options = {
+    context: "http://schema.org",
+    endpoint: "http://localhost:8890/sparql",
+    debug: true
+  };
+  const q = {
+    "proto": [{
+      "id" : "?id",
+      "name": "?name",
+    }],
+    "$where": [
+      "?id rdf:type foaf:Topic",
+      "?id foaf:name ?name"
+    ],
+    "$prefixes": {
+      "foaf": "http://www.semanticweb.org/semanticweb#"
+    },
+    "$limit": 100
+  };
   try {
-    const results = await localClient.query(query);
-    let topics = [];
-    for (const topic of results.results.bindings) {
-      topics.push({ id: topic.subject.value, name: topic.name.value });
-    }
-    res.status(200).json(topics);
+    const out = await sparqlTransformer.default(q, options);
+    res.status(200).json(out);
   } catch (e) {
     console.log(e);
     res.send("Error!");
@@ -129,23 +168,28 @@ router.post("/api/addComment/:uri", async (req, res) => {
 });
 
 router.get("/api/getAgents", async (req, res) => {
+  const options = {
+    // context: "http://schema.org",
+    endpoint: "http://localhost:8890/sparql",
+    debug: true
+  };
+  const q = {
+    "proto": [{
+      "id" : "?id",
+      "name": "?name",
+    }],
+    "$where": [
+      "?id rdf:type foaf:CourseStudent",
+      "?id foaf:name ?name"
+    ],
+    "$prefixes": {
+      "foaf": "http://www.semanticweb.org/semanticweb#"
+    },
+    "$limit": 100
+  };
   try {
-    const results = [
-      {
-        name: 'John Doe',
-        id: 'thisIsUriOfJohnDoe',
-      },
-      {
-        name: 'Jane Doe',
-        id: 'thisIsUriOfJaneDoe',
-      },
-      {
-        name: 'Jimmy Doe',
-        id: 'thisIsUriOfJimmyDoe',
-      },
-    ];
-    console.log(results);
-    res.status(200).json(results);
+    const out = await sparqlTransformer.default(q, options);
+    res.status(200).json(out);
   } catch (e) {
     console.log(e);
     res.send("Error!");
@@ -193,20 +237,28 @@ router.get("/api/getQuestionAssignment/:uri", async (req, res) => {
 });
 
 router.get("/api/questionTypes", async (req, res) => {
-  const query =
-    "SELECT ?subClass ?classLabel WHERE { ?subClass rdfs:subClassOf foaf:QuestionVersion . ?subClass rdfs:label ?classLabel. }";
-  localClient.setQueryFormat(format);
-  localClient.setQueryGraph(graphName);
+  const options = {
+    context: "http://schema.org",
+    endpoint: "http://localhost:8890/sparql",
+    debug: true
+  };
+  const q = {
+    "proto": [{
+      "id" : "?id",
+      "name": "?name",
+    }],
+    "$where": [
+      "?id rdfs:subClassOf foaf:QuestionVersion",
+      "?id rdfs:label ?name"
+    ],
+    "$prefixes": {
+      "foaf": "http://www.semanticweb.org/semanticweb#"
+    },
+    "$limit": 100
+  };
   try {
-    const results = await localClient.query(query);
-    let questionTypes = [];
-    for (const questionType of results.results.bindings) {
-      questionTypes.push({
-        id: questionType.subClass.value,
-        name: questionType.classLabel.value
-      });
-    }
-    res.status(200).json(questionTypes);
+    const out = await sparqlTransformer.default(q, options);
+    res.status(200).json(out);
   } catch (e) {
     console.log(e);
     res.send("Error!");
@@ -281,6 +333,23 @@ router.get("/api/getQuestionVersions/:uri", async (req, res) => {
   }
 });
 
+router.post("/api/createTopic", async (req, res) => {
+  const topicName = req.body.topicName;
+
+  await createTopic(topicName);
+
+  localClient
+    .store(true)
+    .then(result => {
+      console.log(result);
+      res.status(200).json("ok");
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.post("/createNewQuestion", async (req, res) => {
   const author = req.body.author;
   const questionText = req.body.question;
@@ -338,6 +407,38 @@ router.post("/local", (req, res) => {
       res.send("Error!");
     });
 });
+const createTopic = async (topicName) => {
+  ID.config({
+    endpoint: "http://localhost:8890/sparql",
+    graph: "http://www.semanticweb.org/semanticweb",
+    prefix: "http://www.semanticweb.org/semanticweb/Topic/"
+  });
+  const foaf = "http://www.semanticweb.org/semanticweb#";
+  let questionNode = {};
+  await ID.create()
+    .then(questionId => {
+      try {
+        id = questionId;
+        const node = new Node(questionId);
+        questionNode = node;
+        localClient.setOptions(
+          "application/json",
+          { foaf: "http://www.semanticweb.org/semanticweb#" },
+          "http://www.semanticweb.org/semanticweb"
+        );
+        localClient
+          .getLocalStore()
+          .add(new Triple(node, "rdf:type", new Node(foaf + "Topic")));
+        localClient
+          .getLocalStore()
+          .add(new Triple(node, "foaf:name", new Text(topicName)));
+      } catch (e) {
+        console.log(e);
+      }
+    })
+    .catch(console.log);
+  return questionNode;
+};
 const createQuestion = async (author, questionText, topic) => {
   ID.config({
     endpoint: "http://localhost:8890/sparql",
@@ -378,6 +479,7 @@ const createQuestion = async (author, questionText, topic) => {
     .catch(console.log);
   return questionNode;
 };
+
 const createQuestionVersion = async (
   author,
   questionText,
