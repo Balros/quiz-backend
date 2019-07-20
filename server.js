a047336d0c0482910150d452036c70763ccbf87a
adam.grund@gmail.com <adam.grund@gmail.com>
2019-07-20 21:02:45 +0200

    cors address fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const middlewares = [
   bodyParser.urlencoded({extended:true})
 ];
 var corsOptions = {
-  origin: 'https://courses.matfyz.sk:4431',
+  origin: 'https://courses.matfyz.sk',
   // origin: 'http://localhost:3000',
 }
 app.use(cors(corsOptions));
